68cb6bfaab7b190a1073d044861908820ebb8f10
cselig <cselig@uw.edu>
2017-09-04 23:32:44 -0700

    moved stuff around

--- query_all_matchups.py
+++ query_all_matchups.py
@@ -25,7 +25,7 @@ def get_html(p1id, p2id, driver):
 
 
 # parse matchup stats from html
-def parse_html(source):
+def parse_html(source, f, p1id, p2id):
 	soup = BeautifulSoup(source, 'html.parser')
 
 	# get to 'Character Use' section
@@ -35,9 +35,11 @@ def parse_html(source):
 	    data_list = match_up.find_all('td')
 	    # extract text and unpack
 	    char1, char1wins, char2wins, char2 = list(map(lambda x: x.text, data_list))
+	    f.write(char1 + ',' + char1wins + ',' + char2wins + ',' + char2 + ',' + p1id + ',' + p2id + '\n')
 	    if char1 != char2:
 	        MATCHUP_STATS[(char1, char2)] += int(char1wins)
 	        MATCHUP_STATS[(char2, char1)] += int(char2wins)
+	return character_use
 
 
 if __name__ == '__main__':
@@ -45,11 +47,23 @@ if __name__ == '__main__':
 	matchups = pickle.load(open('pickles/matchups.p', 'rb'))
 	driver = webdriver.Firefox()
 	driver.get(URL)
-	for p1id, p2id in matchups:
+	matchup_data_file = open('matchup-data.csv', 'w')
+	matchup_data_file.write('char1,char1wins,char2wins,char2,p1id,p2id\n')
+	for i in range(0, len(matchups)):
+		p1id, p2id = matchups[i]
+		if i % 100 == 0 and i != 0:
+			driver.close()
+			driver = webdriver.Firefox()
+			driver.get(URL)
 		source = get_html(str(p1id), str(p2id), driver)
-		print('HTML for %s vs %s retrieved' % (p1id, p2id))
-		parse_html(source)
-		print('HTML for %s vs %s parsed' % (p1id, p2id))
+		# print('HTML for %s vs %s retrieved' % (p1id, p2id))
+		character_use = parse_html(source, matchup_data_file, str(p1id), str(p2id))
+		html_file_name = 'html_data/' + p1id + 'vs' + p2id + '.html'
+		with open(html_file_name, 'w') as f:
+			f.write(str(character_use))
+		# print('HTML for %s vs %s parsed' % (p1id, p2id))
+		print('%s/%s complete' % (i, len(matchups)))
 	pickle.dump(MATCHUP_STATS, open('pickles/matchup_stats.p', 'wb'))
 	print('Success')
+	matchup_data_file.close()
 	pprint(MATCHUP_STATS)
